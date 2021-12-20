69fc7cf3f9655f29fe9cf42867ca8ea6c14a3245
hosprojektant <hos@DESKTOP-BDOHHHJ.localdomain>
2021-12-20 13:37:12 +0100

    testing menu ver1

--- lib.py
+++ lib.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from logging import NullHandler
+from tkinter.constants import FALSE, LEFT
 import MetaTrader5 as mt5
 import time
 from numpy.lib.function_base import average, median
@@ -8,27 +10,25 @@ import pandas as pd
 import tkinter as tk
 from datetime import date
 from statistics import mean
-from tkinter import messagebox
+from tkinter import Canvas, messagebox
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
-ticker_names=['AUDCAD',
-'AUDJPY',
-'AUDNZD',
-'AUDCHF',
-'AUDUSD',
-'GBPAUD',
-'GBPCAD',
-'GBPJPY'
-]
+import fileworker
+
+
+
+
 data_sell=[]
 data_buy=[]
 data_time=[]
 ticks=[]
 ticks_frame=[]
 data_profit=[]
-
-
+ticker_names_settings=[]
+profit_cnt = 0
+minus_cnt =0
+toolbar=0
 print("MetaTrader5 package author: ",mt5.__author__)
 print("MetaTrader5 package version: ",mt5.__version__)
  
@@ -46,29 +46,40 @@ if not mt5.initialize():
     print("initialize() failed, error code =",mt5.last_error())
     quit()
 
+
+
+
+
 # acount and server information    
-login=1090045834
-password="ECMUGKT6DZ"
-server="FTMO-Server"
+login=fileworker.load_setings("login")
+password=fileworker.load_setings("password")
+server=fileworker.load_setings("server")
+ticker_names=fileworker.load_setings("ticker_names")
+
 
 mt5.login(login,password,server)
 
 
 
 
-def frame_algo(tmp_buy,index_ticker,big_frame_size,tmp_time,plot1):
+
+
+
+
+
+def frame_algo(tmp_buy,index_ticker,tmp_time,plot1):
     frame_buy=[]
     frame_buy_info=[]
     frame_time=[]
     cnt_buy =0
     cnt_sell = 0
-    len_of_frame=100
+    len_of_frame=650
     size = len(tmp_buy)/len_of_frame
     for q in range(len_of_frame):
         frame_buy.append([])
         frame_buy_info.append([])
         frame_time.append([])
-        for i in range(int(size*(q+1)-size),int(size*(q+1))):
+        for i in range(int(size*(q+1)-size),int(size*(q+1)),10):
             frame_buy[q].append(tmp_buy[i])
             frame_time[q].append(tmp_time[i])
         #frame_buy_info[q].append(mean(frame_buy[q])) ## mean
@@ -86,11 +97,11 @@ def frame_algo(tmp_buy,index_ticker,big_frame_size,tmp_time,plot1):
             cnt_buy=0
             
         if cnt_sell==3 and q+1 < len(frame_buy_info):
-            algo_test("sell",q,size,index_ticker,frame_buy[q+1][0],big_frame_size,plot1)
+            algo_test("sell",q,size,index_ticker,frame_buy[q+1][0],plot1)
             ##plot1.plot(frame_time[q+1],frame_buy[q+1],color="red")
             
         if cnt_buy==3 and q+1 < len(frame_buy_info):
-            algo_test("buy",q,size,index_ticker,frame_buy[q+1][0],big_frame_size,plot1)
+            algo_test("buy",q,size,index_ticker,frame_buy[q+1][0],plot1)
             ##plot1.plot(frame_time[q+1],frame_buy[q+1],color="blue")
 
 
@@ -99,27 +110,33 @@ def frame_algo(tmp_buy,index_ticker,big_frame_size,tmp_time,plot1):
 
 
 
-def algo_test(state,q,size,index_ticker,value,big_frame_size,plot1):
-    start = int(size*(q+1))+big_frame_size
+def algo_test(state,q,size,index_ticker,value,plot1):
+    start = int(size*(q))
+    global minus_cnt 
+    global profit_cnt
     if state == "buy":
         for i in range(start,len(data_buy[index_ticker]),20):
-            if value*0.0010+value < data_buy[index_ticker][i]:
+            if value*0.0005+value < data_buy[index_ticker][i]: ## původní 155 159  160 aktualní
                 data_profit.append(data_buy[index_ticker][i]-value) ## profit
-                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="yellow")
+                profit_cnt+=1
+                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="blue")
                 break
-            if -value*0.0010+value > data_buy[index_ticker][i]:
+            if -value*0.0005+value > data_buy[index_ticker][i]: ## 155 původní  150     149 aktualní
                 data_profit.append(data_buy[index_ticker][i]-value) ## jsme v mínusu 
-                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="green")
+                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="yellow")
+                minus_cnt+=1
                 break
     if state == "sell":
          for i in range(start,len(data_buy[index_ticker]),20):
-            if value*0.0010+value < data_buy[index_ticker][i]:
+            if value*0.0005+value < data_buy[index_ticker][i]: ## původní 155 159  160 aktualní
                 data_profit.append(-data_buy[index_ticker][i]+value) ## jsme v mínusu
-                #plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="green")
+                minus_cnt+=1
+                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="green")
                 break
-            if -value*0.0010+value > data_buy[index_ticker][i]:
+            if -value*0.0005+value > data_buy[index_ticker][i]:  ## 155 původní 150   149 aktualní
                 data_profit.append(-data_buy[index_ticker][i]+value) ## profit
-                #plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="yellow")
+                profit_cnt+=1
+                plot1.plot((data_time[index_ticker][start:i]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][start:i],color="red")
                 break
 
 
@@ -129,7 +146,7 @@ def fill_data():
     now = datetime.now()
     timezone = pytz.timezone("Etc/UTC")
     # create 'datetime' objects in UTC time zone to avoid the implementation of a local time zone offset
-    q=30
+    q=40
 
     # 
     if now.month == 1:
@@ -142,7 +159,12 @@ def fill_data():
         utc_from = datetime(now.year, now.month, now.day-q, now.hour, now.minute, tzinfo=timezone)
     
     utc_to = datetime(now.year, now.month, now.day+1, now.hour, now.minute, tzinfo=timezone)
-    
+
+    for i in range(0,len(ticker_names)):
+        if ticker_names[i] == "":
+            ticker_names.pop(i)
+            break
+    print(ticker_names)
 
     # fill data_sell and buy
     for i in range(0,len(ticker_names)):
@@ -156,8 +178,10 @@ def fill_data():
     
 # vygeneruje graf na ploše
 def plot(index_ticker):
-    tmp_buy=[]
-    tmp_time=[]
+    global toolbar
+    if toolbar != 0:
+        toolbar.destroy()
+
     # the figure that will contain the plot
     fig = Figure(figsize = (10, 5),
                  dpi = 150)
@@ -168,29 +192,16 @@ def plot(index_ticker):
     plot1 = fig.add_subplot(111)
   
     # plotting the graph
-    plot1.plot((data_time[index_ticker][::500]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][::500],color="black")
-    avarage = min(data_buy[index_ticker][::500])
-    lengh_of_state = 6
+    plot1.plot((data_time[index_ticker][::2]-data_time[index_ticker][0])/(3600*24),data_buy[index_ticker][::2],color="black")
     first_time = data_time[index_ticker][0]
-    size = len(data_buy[index_ticker])/lengh_of_state
-    for q in range(0,lengh_of_state):
-        for i in range(int((size*(q+1)-size)), int(size*(q+1)),15):
-            tmp_buy.append(data_buy[index_ticker][i])
-            tmp_time.append(data_time[index_ticker][i])
-        
-        value_start= tmp_buy[0]
-        value_end = tmp_buy[-1]
 
-        frame_algo(tmp_buy,index_ticker,int((size*(q+1)-size)),(tmp_time-first_time)/(3600*24),plot1)
+    frame_algo(data_buy[index_ticker],index_ticker,(data_time[index_ticker]-first_time)/(3600*24),plot1)
 
-
-        for i in range(len(tmp_buy)):
-            tmp_buy[i]=avarage
        #########################
-        
-        tmp_buy.clear()
-        tmp_time.clear()
+
     print(sum(data_profit))
+    print(profit_cnt)
+    print(minus_cnt)
     data_profit.clear()
     # creating the Tkinter canvas
     # containing the Matplotlib figure
@@ -208,27 +219,171 @@ def plot(index_ticker):
 
 
 
+
 top = tk.Tk()
 fill_data()
 
 
 top.geometry("1280x720")
 top.state('zoomed') #full screen 
-label_user_name = tk.Label(top, text="User Name")
-label_user_name.place(x=5, y=5)
-entry_user_name = tk.Entry(top, bd =5)
-entry_user_name.place(x=80, y=5)
-label_password = tk.Label(top, text="Password")
-label_password.place(x=5, y=40)
-entry_password = tk.Entry(top, bd =5)
-entry_password.place(x=80, y=40)
+
+
+def clear_text(x):
+    x.delete(0, 'end')
+
+
+def account_settings():
+     
+    # Toplevel object which will
+    # be treated as a new window
+    settings= tk.Toplevel(top)
+    ## section for user server info
+    user_input_name = tk.StringVar(settings)
+    user_input_password = tk.StringVar(settings)
+    user_input_server = tk.StringVar(settings)
+    label_user_name = tk.Label(settings, text="User Name")
+    label_user_name.place(x=5, y=5)
+    entry_user_name = tk.Entry(settings, bd =5,textvariable=user_input_name)
+    entry_user_name.place(x=80, y=5)
+    label_password = tk.Label(settings, text="Password")
+    label_password.place(x=5, y=40)
+    entry_password = tk.Entry(settings, bd =5,textvariable=user_input_password)
+    entry_password.place(x=80, y=40)
+    label_server = tk.Label(settings, text="Server")
+    label_server.place(x=5, y=75)
+    entry_server = tk.Entry(settings, bd =5,textvariable=user_input_server)
+    entry_server.place(x=80, y=75)
+    btn = tk.Button(settings,text ="Save",command = lambda: [clear_text(entry_user_name),clear_text(entry_password),clear_text(entry_server),
+    fileworker.rewrite_remove(user_input_name.get(),0),fileworker.rewrite_remove(user_input_password.get(),1),fileworker.rewrite_remove(user_input_server.get(),2)])
+    global login
+    global password
+    login=fileworker.load_setings("login")
+    password=fileworker.load_setings("password")
+    btn.place(x=100, y=110)
+    ## end of section for user server info
+    
+    
+
+
+
+    # sets the title of the
+    # Toplevel widget
+    settings.title("User info")
+ 
+    # sets the geometry of toplevel
+    settings.geometry("250x150")
+
+
+def remove_account_settings(tickers_gui):
+    for widget in top.winfo_children():
+        widget.destroy()
+    tickers_gui.destroy()
+    tickers_gui= tk.Toplevel(top)
+    tickers_gui.geometry("1280x720")
+    tickers_gui.state('zoomed') #full screen 
+    update_ticker_settings(tickers_gui)
+    fill_data()
+    fill_butons_start()
+    menu()
+
+
+
+def update_ticker_settings(tickers_gui):
+  
+    ticker_names_settings=fileworker.load_setings("settings_names")
+
+    ## removing items that dont need to be add bs they already are in ticker_names
+    for i in ticker_names[:]:
+        if i in ticker_names_settings:
+            ticker_names_settings.remove(i)
+
+    # adding butons for adding
+    x_axis_tickers=20
+    y_axis_tickers=50
+    print(y_axis_tickers,"Y")
+    print(x_axis_tickers,"X")
+    for i in ticker_names_settings:
+        add_ticker_setings(i,x_axis_tickers,y_axis_tickers,tickers_gui)
+        y_axis_tickers+=30
+        if y_axis_tickers > 900:
+            x_axis_tickers+=100
+            y_axis_tickers=50
+    # adding butons for removing
+    x_axis_tickers=800
+    y_axis_tickers=50
+    for i in ticker_names:
+        if i != "":
+            remove_ticker_setings(i,x_axis_tickers,y_axis_tickers,tickers_gui)
+            y_axis_tickers+=30
+            if y_axis_tickers > 900:
+                x_axis_tickers+=100
+                y_axis_tickers=50
+
+    B= tk.Button(tickers_gui,text ="Update",command = lambda: remove_account_settings(tickers_gui) )
+    B.place(x=450, y=20,width=100, height=30)
+
+
+def add_ticker_settings_action(ticker_name):
+    global ticker_names
+    global ticker_names_settings
+    fileworker.add_new_ticker(ticker_name)
+    ticker_names_settings=fileworker.load_setings("settings_names")
+    ticker_names=fileworker.load_setings("ticker_names")
+
+def remove_ticker_settings_action(ticker_name):
+    global ticker_names
+    global ticker_names_settings
+    fileworker.rewrite_remove(ticker_name,3) # 3 for ticker_name
+    ticker_names_settings=fileworker.load_setings("settings_names")
+    ticker_names=fileworker.load_setings("ticker_names")
+
+
+def add_ticker_setings(ticker_name,x_axis_tickers,y_axis_tickers,gui):
+    B = tk.Button(gui,text =ticker_name, command = lambda: add_ticker_settings_action(ticker_name))
+    B.place(x=x_axis_tickers, y=y_axis_tickers, width=100, height=30)
+
+def remove_ticker_setings(ticker_name,x_axis_tickers,y_axis_tickers,gui):
+    B = tk.Button(gui,text =ticker_name, command = lambda: remove_ticker_settings_action(ticker_name))
+    B.place(x=x_axis_tickers, y=y_axis_tickers, width=100, height=30)
+
+
+def add_tickers():  
+    tickers_gui= tk.Toplevel(top)
+    tickers_gui.geometry("1280x720")
+    tickers_gui.state('zoomed') #full screen 
+    update_ticker_settings(tickers_gui)
+
+
+
+ 
+ 
+def menu():
+    ##section menu
+    menu = tk.Menu(top)
+    top.config(menu=menu)
+    fileMenu = tk.Menu(menu)
+    menu.add_cascade(label="Settings", menu=fileMenu)
+    fileMenu.add_command(label="Account", command=account_settings)
+    fileMenu.add_command(label="Add tickers", command=add_tickers)
+    editMenu = tk.Menu(menu)
+    menu.add_cascade(label="Edit", menu=editMenu)
+    ## end of menu section
+
+
+menu()
+
+
+
+
+
+
+
 
 
 # přidáme tlačítka na plohu společně s jejich vlastnostmi 
 def fill_butons(ticker_name,x_axis_tickers,index_ticker,y_axis_tickers):
-
-    B = tk.Button(text =ticker_name, command = lambda: plot(index_ticker))
-    B.place(x=x_axis_tickers, y=y_axis_tickers)
+    B = tk.Button(top,text =ticker_name, command = lambda: plot(index_ticker))
+    B.place(x=x_axis_tickers, y=y_axis_tickers,width=100, height=30)
 
 
 def fill_butons_start():
@@ -237,7 +392,7 @@ def fill_butons_start():
     y_axis_tickers=5
     for i in ticker_names:
         fill_butons(i,x_axis_tickers,x,y_axis_tickers)
-        x_axis_tickers+=60
+        x_axis_tickers+=100
         if x_axis_tickers > 1500:
             x_axis_tickers=400
             y_axis_tickers+=30
--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-from datetime import datetime
-import MetaTrader5 as mt5
-import time
-import pytz
-import pandas as pd
-import tkinter as tk
-from tkinter import messagebox
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
-NavigationToolbar2Tk)
-plt.plot(10, 10)
-plt.tight_layout()
+   def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+        menubar = frame.menubar(self)
+        self.configure(menu=menubar)
